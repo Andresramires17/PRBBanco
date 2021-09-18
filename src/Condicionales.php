3316c1495a7de659f28cf596744c142598cc2429
andresramires17 <andresramires17@gmail.com>
2021-09-18 16:02:32 -0500

    condicionales

---
+++ src/Condicionales.php
@@ -0,0 +1,23 @@
+<?php
+     $Var1 = 10;
+     $Var2 = 10;
+    // > mayor que
+        // menor que  < 
+        // igual que ==
+        // >=  mayor o igual 
+        // Menor o igual <=
+     if ($Var1 < $Var2) {
+        
+        echo 'si es menor';
+        
+     }
+     elseif ($Var1 == $Var2) {
+         echo "El número $Var1 es igual al número $Var2 ";
+     }
+
+     else {
+         echo 'no es menor';
+     }
+
+
+?>
